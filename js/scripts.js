a4d8a45b6e3e165be42314896de550a0c80e78b5
Jieun Kang <jieunkang101@gmail.com>
2020-01-17 11:31:53 -0800

    add panel for user output and custom styles with CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  $("#survey").submit(function(event) {
+    event.preventDefault();
+    var nameInput = $("input#name").val();
 
+    $(".name").text(nameInput);
 
+  });
 });
