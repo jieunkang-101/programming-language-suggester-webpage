d72bb29f194960dc6ce64ea0c281f8ce372b4e14
Jieun Kang <jieunkang101@gmail.com>
2020-01-17 16:40:13 -0800

    practice adding branching

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
     event.preventDefault();
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
+    var company = $("input:radio[name=groupCompany]:checked").val();
+    var position = document.getElementById("position").val();
+    var choice = $("input:radio[name=groupChoice]:checked").val();
+
+    if (position === "frontend") {
+      $("#outputCsharp").show();
+    }
 
     $(".firstName").text(firstNameInput);
     $(".lastName").text(lastNameInput);
 
-   $(".panel").show(); 
+  //  $(".panel").show(); 
 
   });
 });
