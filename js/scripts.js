db9b3e3bef9a54c9eec8816674e73230adfdfcde
Jieun Kang <jieunkang101@gmail.com>
2020-01-17 17:06:14 -0800

    change select box form to radio button form

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
     var company = $("input:radio[name=groupCompany]:checked").val();
-    var position = document.getElementById("position").val();
+    var position = $("input:radio[name=groupPosition]:checked").val();
     var choice = $("input:radio[name=groupChoice]:checked").val();
 
-    if (position === "frontend") {
-      $("#outputCsharp").show();
+    if (position === "frontEnd") {
+      $("#outputCsharp").show(); 
+
+      // $("#outputCsharp").show();
     }
 
     $(".firstName").text(firstNameInput);
